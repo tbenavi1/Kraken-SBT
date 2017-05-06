c24a668e0f96882f24d3b57f1d4fb91ce84781e4
Timothy Rhyker Ranallo-Benavidez <tbenavi1@jhmi.edu>
2017-05-06 11:38:22 -0400

    Update Kraken-SBT.py

--- Kraken-SBT.py
+++ Kraken-SBT.py
@@ -35,14 +35,14 @@ class BloomFilter:
 #recall: command to generate name_ftpdirpaths from assembly_summary.txt (ftp://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria/assembly_summary.txt) is given below:
 #awk -F "\t" '$12=="Complete Genome" && $11=="latest"{print $8, $20}' assembly_summary.txt > name_ftpdirpaths
 #each line of name_ftpdirpaths is space delimited; everything before the last space is the taxon name ($8), everything after the last space is the ftpdirpath ($20)
-def get_taxonid_to_dumpsfilenames(name_ftpdirpaths_filename): 
-	#search through all the data files and create a dictionary that maps taxonids to dumpsfilenames (dumps files are generated from Jellyfish and contain kmers)
-	taxonid_to_dumpsfilenames = defaultdict(list) #a given taxonid may map to multiple dumpsfiles, thus each value in the dictionary is a list
+def get_taxonid_to_readfilenames(name_ftpdirpaths_filename): 
+	#search through all the data files and create a dictionary that maps taxonids to readfilenames
+	taxonid_to_readfilenames = defaultdict(list) #a given taxonid may map to multiple readfiles, thus each value in the dictionary is a list
 	for line in open(name_ftpdirpaths_filename):
 		splits = line.strip().split(' ') #split on each of the spaces
 		name = ' '.join(splits[:-1]) #concatenate everything before the last space
 		ftpdirpath = splits[-1] #everything after the last space
-		dumpsfilename = './Bacteria_Genomes/' + ftpdirpath.split('/')[-1] + '_genomic.fna.gz.dumps'
+		readfilename = './Bacteria_Genomes/' + ftpdirpath.split('/')[-1] + '_genomic.fna.gz'
 		name_to_listtaxonid = ncbi.get_name_translator([name]) #a dictionary with name as key and [taxonid] as value
 		listtaxonid = [taxonid for [taxonid] in name_to_listtaxonid.values()]
 		if listtaxonid == []:
@@ -53,17 +53,17 @@ def get_taxonid_to_dumpsfilenames(name_ftpdirpaths_filename):
 				name_to_listtaxonid = ncbi.get_name_translator([name])
 				listtaxonid = [taxonid for [taxonid] in name_to_listtaxonid.values()]
 				taxonid = listtaxonid[0]
-				taxonid_to_dumpsfilenames[taxonid].append(dumpsfilename)
+				taxonid_to_readfilenames[taxonid].append(readfilename)
 			if name == 'Mycobacterium intracellulare MOTT-64': #upon further inspection, this name in assembly_summary.txt is curiously absent from NCBI
 				print(name+' is not in the NCBI database')
 				sys.stdout.flush()
 		else:
 			taxonid = listtaxonid[0]
-			taxonid_to_dumpsfilenames[taxonid].append(dumpsfilename)
-	return taxonid_to_dumpsfilenames
+			taxonid_to_readfilenames[taxonid].append(readfilename)
+	return taxonid_to_readfilenames
 
-def bf_from_bvfilename(bvfilename):
-	f = open(bvfilename, 'rb')
+def bf_from_bvfilename(bv_filename):
+	f = open(bv_filename, 'rb')
 	bitvector = bitarray()
 	bitvector.fromfile(f)
 	f.close()
@@ -71,58 +71,97 @@ def bf_from_bvfilename(bvfilename):
 	return bf
 
 def get_tree(name_ftpdirpaths_filename,num_taxons = 0):
-	taxonid_to_dumpsfilenames = get_taxonid_to_dumpsfilenames(name_ftpdirpaths_filename)
+	taxonid_to_readfilenames = get_taxonid_to_readfilenames(name_ftpdirpaths_filename)
 	
 	#get the desired number of unique taxonids, in order to create the phylogeny tree
-	taxonids = taxonid_to_dumpsfilenames.keys() #length is 6,740 as expected (6,741 minus the one not in NCBI)
+	taxonids = taxonid_to_readfilenames.keys() #length is 6,740 as expected (6,741 minus the one not in NCBI)
 	taxonids = list(set(taxonids)) #4,526 taxonids are unique
 	if num_taxons != 0:
 		taxonids = taxonids[:num_taxons] #smaller set of taxonids for tree construction and testing
 	
 	#return desired phylogeny tree
-	return taxonid_to_dumpsfilenames, ncbi.get_topology(taxonids) #5,360 total nodes for full dataset
+	return taxonid_to_readfilenames, ncbi.get_topology(taxonids) #5,360 total nodes for full dataset
 
-def construct_bloomfilters(tree):
-		
-	edited_names = []
+def write_descendantfiles(tree):
 	i=0
 	numnodes = len(list(tree.traverse()))
-	for node in tree.traverse('postorder'): #traverse each node in the tree from leaf level upward to root level
+	for node in tree.traverse():
 		i+=1
 		taxonid = int(node.name)
 		name = ncbi.translate_to_names([taxonid])[0]
 		print('Node ' + str(i) + ' out of ' + str(numnodes) + ': ' + name)
 		sys.stdout.flush()
-		if "/" in name:
-			edited_names.append(name)
-		edited_name = name.replace(' ', '_').replace('/', '_') #replace spaces and / with underscores so that it can be a filename, e.g. Acinetobacter calcoaceticus/baumannii complex
+		edited_name = name.replace(' ', '_').replace('/', '_')  #replace spaces and / with underscores so that it can be a filename, e.g. Acinetobacter calcoaceticus/baumannii complex
+		f = open("editednames", 'a')
+		f.write(edited_name + '\n')
+		f.close()
+		descendantfilenames_filename = edited_name + '.descendantfilenames'
+		if taxonid in taxonid_to_readfilenames:
+			readfilenames = taxonid_to_readfilenames[taxonid]
+			numfiles = len(readfilenames)
+			j = 0
+			for readfilename in readfilenames:
+				j+= 1
+				print('Node ' + str(i) + ' out of ' + str(numnodes) + ': ' + name + '; File ' + str(j) + ' out of ' + str(numfiles))
+				sys.stdout.flush()
+				f = open(descendantfilenames_filename,'a')
+				f.write(readfilename + '\n')
+				f.close()
+		if not node.is_leaf():
+			num_descendants = len(node.get_descendants())
+			k = 0
+			for descendant in node.get_descendants():
+				k += 1
+				descendant_taxonid = int(descendant.name)
+				descendant_name = ncbi.translate_to_names([descendant_taxonid])[0]
+				print('Node ' + str(i) + ' out of ' + str(numnodes) + ': ' + name + '; Descendant Node ' + str(k) + ' out of ' + str(num_descendants) + ': ' + descendant_name)
+				sys.stdout.flush()
+				descendant_filenames = taxonid_to_readfilenames[descendant_taxonid]
+				descendant_numfiles = len(descendant_filenames)
+				l = 0
+				for descendant_filename in descendant_filenames:
+					l+= 1
+					print('Node ' + str(i) + ' out of ' + str(numnodes) + ': ' + name + '; Descendant Node ' + str(k) + ' out of ' + str(num_descendants) + ': ' + descendant_name + '; Descendant File ' + str(l) + ' out of ' + str(descendant_numfiles))
+					sys.stdout.flush()
+					f = open(descendantfilenames_filename,'a')
+					f.write(descendant_filename + '\n')
+					f.close()
+
+def read_bloomfiltersizes(bloomfiltersizes_filename):
+	bloomfiltersizes = {}
+	for line in open(bloomfiltersizes_filename):
+		edited_name, size = line.strip().split(' ')
+		bloomfiltersizes[edited_name] = size
+	return bloomfiltersizes
+	
+def construct_bloomfilters(tree, bloomfiltersizes):
+	i=0
+	numnodes = len(list(tree.traverse()))
+	for node in tree.traverse():
+		i+=1
+		taxonid = int(node.name)
+		name = ncbi.translate_to_names([taxonid])[0]
+		print('Node ' + str(i) + ' out of ' + str(numnodes) + ': ' + name)
+		sys.stdout.flush()
+		edited_name = name.replace(' ', '_').replace('/', '_')
+		#descendantfilenames_filename = edited_name + '.descendantfilenames'
+		descendantfilenames_filename = edited_name + '.readfiles'
 		bv_filename = edited_name + '.bv'
 		if False: #os.path.exists(bv_filename):
-			print(bv_filename + ' already exists. Loading...')
+			print(bv_filename + ' already exists.')
 			sys.stdout.flush()
-			node.bf = bf_from_bvfilename(bv_filename)
 		else:
-			node.kmers = []
-			if taxonid in taxonid_to_dumpsfilenames:
-				dumpsfilenames = taxonid_to_dumpsfilenames[taxonid]
-				numfiles = len(dumpsfilenames)
-				j = 0
-				for dumpsfilename in dumpsfilenames:
-					j+= 1
-					print('Node ' + str(i) + ' out of ' + str(numnodes) + ': ' + name + '; File ' + str(j) + ' out of ' + str(numfiles))
-					sys.stdout.flush()
-					for line in open(dumpsfilename):
-						kmer = line.strip().split(' ')[0]
-						node.kmers.append(kmer)
-			if not node.is_leaf():
-				children = node.children
-				for child in children:
-					node.kmers.extend(child.kmers)
-					delattr(child, 'kmers')
-			size = int(round(len(node.kmers) * 4.25,-3))
+			size = bloomfiltersizes[edited_name]
+			size = int(round(size * 4.25,-3))
 			node.bf = BloomFilter(size, num_hashes = 3)
-			for kmer in node.kmers:
-				node.bf.add(kmer)
+			j = 0
+			for line in open(descendantfilenames_filename):
+				j += 1
+				descendant_filename = line.strip()
+				print('Reading file ' + str(j))
+				for line2 in open(descendant_filename):
+					kmer = line2.strip().split(' ')[0]
+					node.bf.add(kmer)
 			f = open(bv_filename, 'wb')
 			node.bf.bv.tofile(f)
 			f.close()
@@ -232,7 +271,7 @@ def query_tree(tree, query, threshold):
 	
 	#initialize variables
 	responses = {}
-	num_threads = 3
+	num_threads = 5
 	threads = []
 	queueLock = threading.Lock()
 	counterLock = threading.Lock()
@@ -276,7 +315,9 @@ if __name__=="__main__":
 	if command == "bloomfilters":
 		#construct the bloomfilters (only necessary for the first time building the database)
 		#actually, the end user never needs to perform this step, since they will download the bloom filters from the beginning
-		construct_bloomfilters(tree)
+		#bloomfiltersizes = read_bloomfiltersizes('bloomfiltersizes')
+		bloomfiltersizes = read_bloomfiltersizes('bloomfiltersizes2')
+		construct_bloomfilters(tree, bloomfiltersizes)
 	
 	if command == "query":
 		querytaxonid = sys.argv[3]
